d7d535405edfc2fe462eea06bd107edd0da3a0a0
Jens Hausherr <jens@hausherr.net>
2025-03-16 14:11:28 +0100

    Remove packer config

--- packer.tf
+++
@@ -1,2 +0,0 @@
-endpoint_url: https://api.cloud.hashicorp.com/packer/2023-01-01/terraform-cloud/validation/46dcea71-2499-48de-8731-5b8c777dda50
-HMAC: e9df8d92848e1340c62e884941c7e1e95107248870481f625bb86b8ec49b5212
