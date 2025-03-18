ea2792cc5663e5a33052fe1505f721f27fbe4668
Jens Hausherr <jens@hausherr.net>
2025-03-18 05:51:49 +0100

    Install ca-certificates

--- images/minecraft-java/scripts/system_setup.sh
+++ images/minecraft-java/scripts/system_setup.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env sh
 set -x
 sudo apt-get update
-sudo apt-get install -y openjdk-21-jre-headless curl jq
+sudo apt-get install -y ca-certificates openjdk-21-jre-headless curl jq
 useradd -m -s /bin/bash minecrafter 
+
+tic -x -o /usr/share/terminfo /tmp/ghostty.terminfo
