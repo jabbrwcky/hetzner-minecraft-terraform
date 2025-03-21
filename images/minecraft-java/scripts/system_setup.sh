9e91d5b8c0091f452fb3f59ebd18fa955980e24e
Jens Hausherr <jens@hausherr.net>
2025-03-21 17:07:30 +0100

    Install tailscale

--- images/minecraft-java/scripts/system_setup.sh
+++ images/minecraft-java/scripts/system_setup.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env sh
 set -x
+curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/noble.noarmor.gpg | sudo tee /usr/share/keyrings/tailscale-archive-keyring.gpg >/dev/null
+curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/noble.tailscale-keyring.list | sudo tee /etc/apt/sources.list.d/tailscale.list
+
 sudo apt-get update
-sudo apt-get install -y ca-certificates openjdk-21-jre-headless curl jq
+sudo apt-get install -y ca-certificates openjdk-21-jre-headless curl jq tailscale
 useradd -m -s /bin/bash minecrafter 
 
 tic -x -o /usr/share/terminfo /tmp/ghostty.terminfo
+
+
